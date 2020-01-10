54bb31519cf427611baaf38832cbae6924ea4481
root <root@localhost.localdomain>
2020-01-10 17:00:02 +0000

    17:00

--- create_vm.bat
+++
@@ -1,8 +0,0 @@
-docker-machine rm managervm -y
-docker-machine create --driver hyperv managervm
-docker-machine rm worker1vm -y
-docker-machine create --driver hyperv worker1vm
-docker-machine rm worker2vm -y
-docker-machine create --driver hyperv worker2vm
-docker-machine rm worker3vm -y
-docker-machine create --driver hyperv worker3vm
