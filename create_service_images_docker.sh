7adadff5fac07ab096f262d922c7117aa6576d4e
root <root@localhost.localdomain>
2020-01-10 11:55:48 +0000

    Add JenkinsFile

--- create_service_images_docker.sh
+++ create_service_images_docker.sh
@@ -27,3 +27,18 @@ docker rmi services/test
 cd services/test-service
 docker build --build-arg JAR_FILE=build/libs/*.jar -t services/test .
 cd ../..
+docker login --username=vee97 --password=RabbitVlad1997
+docker tag services/discovery vee97/discovery
+docker push vee97/discovery
+docker tag services/gateway vee97/gateway
+docker push vee97/gateway
+docker tag services/result vee97/result
+docker push vee97/result
+docker tag services/session vee97/session
+docker push vee97/session
+docker tag services/task-executor vee97/task-executor
+docker push vee97/task-executor
+docker tag services/task vee97/task
+docker push vee97/task
+docker tag services/test vee97/test
+docker push vee97/test
