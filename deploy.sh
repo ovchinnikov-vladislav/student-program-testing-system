3397a556a0d3c46726192e14b058120bac0db449
root <root@localhost.localdomain>
2020-01-13 20:15:26 +0000

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,21 @@
 docker-machine start managervm
 docker-machine start worker1vm
+docker-machine start worker2vm
+docker-machine start worker3vm
+docker-machine start worker4vm
 docker-machine ls
 docker-machine ssh managervm "docker node ls"
 docker-machine scp docker-compose.yml managervm:~
 docker-machine ssh managervm "mkdir pg-init-scripts"
 docker-machine ssh worker1vm "mkdir pg-init-scripts"
+docker-machine ssh worker2vm "mkdir pg-init-scripts"
+docker-machine ssh worker3vm "mkdir pg-init-scripts"
+docker-machine ssh worker4vm "mkdir pg-init-scripts"
 docker-machine scp pg-init-scripts/create-multiple-postgresql-databases.sh managervm:~/pg-init-scripts
 docker-machine scp pg-init-scripts/create-multiple-postgresql-databases.sh worker1vm:~/pg-init-scripts
+docker-machine scp pg-init-scripts/create-multiple-postgresql-databases.sh worker2vm:~/pg-init-scripts
+docker-machine scp pg-init-scripts/create-multiple-postgresql-databases.sh worker3vm:~/pg-init-scripts
+docker-machine scp pg-init-scripts/create-multiple-postgresql-databases.sh worker4vm:~/pg-init-scripts
 docker-machine ssh managervm "docker stack rm services"
 docker-machine ssh managervm "docker stack deploy -c docker-compose.yml services"
 docker-machine ssh managervm "docker stack ps services"
