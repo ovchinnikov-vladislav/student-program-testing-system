475f070200bf17e490c84cb092a5d29ad08ecdc0
root <root@localhost.localdomain>
2020-01-16 17:32:57 +0000

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 echo
 echo 'START DOCKER-MACHINE managervm'
 docker-machine start managervm
+echo
 echo 'START DOCKER-MACHINE worker1vm'
 docker-machine start worker1vm
+echo
 echo 'START DOCKER-MACHINE worker2vm'
 docker-machine start worker2vm
+echo
 echo 'START DOCKER-MACHINE worker3vm'
 docker-machine start worker3vm
+echo
 docker-machine ls
 docker-machine ssh managervm "docker node ls"
 docker-machine scp docker-compose.yml managervm:~
@@ -18,12 +22,17 @@ docker-machine scp pg-init-scripts/create-multiple-postgresql-databases.sh manag
 docker-machine scp pg-init-scripts/create-multiple-postgresql-databases.sh worker1vm:~/pg-init-scripts
 docker-machine scp pg-init-scripts/create-multiple-postgresql-databases.sh worker2vm:~/pg-init-scripts
 docker-machine scp pg-init-scripts/create-multiple-postgresql-databases.sh worker3vm:~/pg-init-scripts
+echo
 echo 'REMOVING STACK services FOR CLUSTER'
 docker-machine ssh managervm "docker stack rm services"
+echo
 echo 'REMOVING NETWORK webnet FOR CLUSTER'
 docker-machine ssh managervm "docker network rm webnet"
+echo
 echo 'CREATING NETWORK webnet FOR CLUSTER'
 docker-machine ssh managervm "docker network create --driver overlay webnet"
+echo
 echo 'DEPLOY services FOR CLUSTER'
 docker-machine ssh managervm "docker stack deploy -c docker-compose.yml services"
+echo
 docker-machine ssh managervm "docker stack ps services"
