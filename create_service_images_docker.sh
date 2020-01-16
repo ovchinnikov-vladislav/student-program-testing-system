e4a906e2cf600fbaa07a86fa4a735eacad5d4985
root <root@localhost.localdomain>
2020-01-16 14:54:42 +0000

    change create_service_images_docker.sh

--- create_service_images_docker.sh
+++ create_service_images_docker.sh
@@ -40,6 +40,7 @@ echo 'DOCKERIZING task-service'
 cd services/task-service
 docker build --build-arg JAR_FILE=build/libs/*.jar -t services/task .
 cd ../..
+echo
 echo 'REMOVING DOCKER IMAGE services/test'
 docker rmi services/test
 echo 'DOCKERIZING test-service'
@@ -53,30 +54,38 @@ echo 'DOCKERIZING front-end'
 cd services/front-end
 docker build --build-arg JAR_FILE=build/libs/*.jar -t services/front-end .
 cd ../..
+echo
 echo 'START PUSH IMAGES TO DOCKER HUB'
 docker login --username=vee97 --password=RabbitVlad1997
 docker tag services/discovery vee97/discovery
 echo 'PUSH vee97/discovery'
 docker push vee97/discovery
 docker tag services/gateway vee97/gateway
+echo
 echo 'PUSH vee97/gateway'
 docker push vee97/gateway
 docker tag services/result vee97/result
+echo
 echo 'PUSH vee97/result'
 docker push vee97/result
 docker tag services/session vee97/session
+echo
 echo 'PUSH vee97/session'
 docker push vee97/session
 docker tag services/task-executor vee97/task-executor
+echo
 echo 'PUSH vee97/task-executor'
 docker push vee97/task-executor
 docker tag services/task vee97/task
+echo
 echo 'PUSH vee97/task'
 docker push vee97/task
 docker tag services/test vee97/test
+echo
 echo 'PUSH vee97/test'
 docker push vee97/test
 docker tag services/front-end vee97/front-end
+echo
 echo 'PUSH vee97/front-end'
 docker push vee97/front-end
 echo
